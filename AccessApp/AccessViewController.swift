c44eee1dec8dc0b16054f847c123500e49b76524
Dmitrandir <dim9107@mail.ru>
2022-12-13 22:04:00 +0300

    Delete Storyboard. Add structure Marks & Extensions

---
+++ AccessApp/AccessViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  AccessApp
+//
+//  Created by Дмитрий Солопов on 13.12.2022.
+//
+
+import UIKit
+
+//MARK: - AccessViewController
+final class AccessViewController: UIViewController {
+    
+    //MARK: Override method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
+//MARK: - Settings View
+private extension AccessViewController {
+    func setupView() {
+        
+    }
+}
+
+//MARK: - Setting
+private extension AccessViewController {
+    func addSubViews() {
+        
+    }
+}
+
+//MARK: - Layout
+private extension AccessViewController {
+    func setupLayout() {
+        
+    }
+}
